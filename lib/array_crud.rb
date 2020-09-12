748a624900c76736d22d5ba909f024697bb0ea7d
Aiden Marques <aiden.marques94@gmail.com>
2020-09-12 20:26:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  my_array = ["aiden","meaghan","chloe","jimmy"]
+  my_array=["aiden","meaghan","chloe","jimmy"]
 end
 
 def add_element_to_end_of_array(array, element)
